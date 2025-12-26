b5154f18664cbbcfd3c498b630c4b0e434af6bd3
Negese Zeleke <zelekenegese@gmail.com>
2025-12-26 02:13:23 -0500

    second commite

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,21 +12,9 @@ export const metadata: Metadata = {
   description: "Management system for childcare enrollment, services, and scheduling",
   generator: "v0.app",
   icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
+   
+      
+     
   },
 }
 
